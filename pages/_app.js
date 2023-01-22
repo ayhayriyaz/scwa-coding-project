d6a65bda914bd44e3cc0b6820b5e2f9cc3ba6c31
yahya <yahyariyaz@yahoo.com>
2023-01-21 19:36:48 -0500

    commit 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,44 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${searchTerm}`);
+      setSearchResults(response.data.items.slice(0,3));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Search books by title"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <button type="submit" onClick={handleSearch}>
+          Search
+        </button>
+      </form>
+      {searchResults.map(book => (
+        <div key={book.id}>
+          <h2>{book.volumeInfo.title}</h2>
+          <h3>{book.volumeInfo.subtitle}</h3>
+          <p>Author(s): {book.volumeInfo.authors.join(', ')}</p>
+          <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Search;
